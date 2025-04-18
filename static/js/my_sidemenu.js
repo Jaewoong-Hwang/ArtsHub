cf4447e2657430d0c6519ea4d0147a513ab9fd79
JiwebD <gkdlfnzo12@naver.com>
2025-04-18 12:49:04 +0900

    [style] : 마이페이지_회원정보수정콘텐트 완V0.4

--- static/js/my_sidemenu.js
+++ static/js/my_sidemenu.js
@@ -6,7 +6,9 @@ $(function () {
     "content-userinfo_Authentication",
     "content-funding",
     "content-inquiry",
-    "content-project"
+    "content-project",
+    "content-logout",
+    "content-userinfo_update"
   ];
 
   $(".menu-item").click(function () {
@@ -24,5 +26,23 @@ $(function () {
         $("#" + id).hide();
       }
     });
+  });
+});
+
+
+/**
+ * 회원정보 수정_인증 버튼누르면 회원정보 수정페이지 show
+ */
+$(document).ready(function () {
+  $('#btn-password-check').click(function (e) {
+    e.preventDefault(); // 기본 form 전송 막기
+
+    // 콘텐츠 전환
+    $('#content-userinfo_Authentication').hide();
+    $('#content-userinfo_update').show();
+
+    // 선택사항: 메뉴 active 클래스 변경
+    $('.menu-item').removeClass('active');
+    $('.menu-item').eq(0).addClass('active'); // 첫 번째 메뉴 항목에 active 설정
   });
 });

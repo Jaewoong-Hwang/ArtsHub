b12b98969a18cb09b88fc2184300ff25a0a6e6ad
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-05-21 09:23:45 +0900

    [style] project component 정리(sidebar, header)

--- frontend/src/features/project/projectcreate/pages/js/info.js
+++ frontend/src/features/project/projectcreate/pages/js/info.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // 현재 페이지에 따라 사이드바 active 처리
-  const currentPage = location.pathname.split('/').pop();
-  const navLinks = document.querySelectorAll('.sidebar li');
-
-  navLinks.forEach((li) => {
-    const link = li.querySelector('a');
-    if (link && link.getAttribute('href') === currentPage) {
-      li.classList.add('active');
-    } else {
-      li.classList.remove('active');
-    }
-  });
 
   // 임시 저장 버튼 클릭 시 모달 띄우기
   const saveButton = document.querySelector('.btn.outline');

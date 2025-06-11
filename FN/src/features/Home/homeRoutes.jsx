7f1d5d14e71bfbd8090579a3c4cc44a1b9c5b328
JiwebD <145487843+JiwebD@users.noreply.github.com>
2025-06-11 14:40:22 +0900

    Revert "Feature/bn user security"

--- FN/src/features/Home/homeRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Route } from 'react-router-dom';
-import Home from './pages/Home';
-
-export const HomeRoutes = () => [
-
-     <Route key="main" path="/" element={<Home />} />,
-];
--- FN/src/features/admin/adminRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Route } from 'react-router-dom';
-import AdminDashboard from './pages/AdminDashboard';
-
-export const AdminRoutes = () => (
-  <>
-    <Route path="/admin" element={<AdminDashboard />} />
-  </>
-);

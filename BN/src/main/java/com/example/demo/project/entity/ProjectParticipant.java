dd7cb27a79dff643f860a87818fef34a9a666cd2
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-06-12 03:54:22 +0900

    [feature] project card

---
+++ BN/src/main/java/com/example/demo/project/entity/ProjectParticipant.java
@@ -0,0 +1,4 @@
+package com.example.demo.project.entity;
+
+public class ProjectParticipant {
+}
---
+++ BN/src/main/java/com/example/demo/project/repository/ProjectParticipantRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.project.repository;
+
+public class ProjectParticipantRepository {
+}

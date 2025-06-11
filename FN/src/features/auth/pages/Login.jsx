064b68fbc06d9c9d65b1c641c4d51130c794e9bd
JwebD <gkdlfnzo12@naver.com>
2025-06-11 16:25:47 +0900

    [sytyle] 수정

--- FN/src/features/auth/pages/Login.jsx
+++ FN/src/features/auth/pages/Login.jsx
@@ -1,7 +1,9 @@
+// Login.jsx (고쳐진 CSS 모듈 클래스 적용)
+
 import React, { useState } from "react";
 import axios from "axios";
 import "../../../assets/styles/reset.css";
-import styles from "./css/login/login.module.css"; // 모듈 CSS import
+import styles from "./css/login/login.module.css";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -29,7 +31,7 @@ function Login() {
     <div className={styles.loginContainer}>
       <div className={styles.mainLogoRow}>
         <a href="/">
-          <img src="/static/img/mainlogo.png" alt="로고" className={styles.mainLogo} />
+          <img src="/static/img/mainlogo.png" alt="로고" className={styles.mainlogo} />
         </a>
       </div>
 
@@ -40,7 +42,7 @@ function Login() {
             placeholder="이메일 입력"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className={styles.input}
+            className={styles.customInput}
           />
         </div>
 
@@ -50,11 +52,11 @@ function Login() {
             placeholder="비밀번호 입력"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
+            className={styles.customInput}
           />
         </div>
 
-        <div className={styles.checkboxRow}>
+        <div className={styles.cb}>
           <input type="checkbox" id="login" />
           <label htmlFor="login" className={styles.checkboxLabel}>
             로그인 유지
@@ -63,10 +65,9 @@ function Login() {
 
         {errorMsg && <div className={styles.errorMsg}>{errorMsg}</div>}
 
-<<<<<<< HEAD
         <div className="row mt-4">
           <div className="col">
-            <button className="btn1 btn-custom" onClick={handleLogin}>
+            <button className={styles.btn1} onClick={handleLogin}>
               로그인
             </button>
           </div>
@@ -74,68 +75,41 @@ function Login() {
 
         <div className="row">
           <div className="col">
-            <button className="btn2 btn-custom" onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/kakao"}>
-              <img src="/static/img/kakao.png" alt="카카오" className="kakao" /> 카카오로 시작하기
+            <button
+              className={styles.btn2}
+              onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/kakao"}
+            >
+              <img src="/static/img/kakao.png" alt="카카오" className={styles.kakao} /> 카카오로 시작하기
             </button>
           </div>
         </div>
 
         <div className="row">
           <div className="col">
-            <button className="btn3 btn-custom" onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/naver"}>
-              <img src="/static/img/naver.png" alt="네이버" className="naver" /> 네이버로 시작하기
+            <button
+              className={styles.btn3}
+              onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/naver"}
+            >
+              <img src="/static/img/naver.png" alt="네이버" className={styles.naver} /> 네이버로 시작하기
             </button>
-            </div>
           </div>
+        </div>
 
         <div className="row">
           <div className="col">
-            <button className="btn4 btn-custom" onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/google"}>
-              <img src="/static/img/google.png" alt="구글" className="google" /> 구글로 시작하기
+            <button
+              className={styles.btn4}
+              onClick={() => window.location.href = "http://localhost:8090/oauth2/authorization/google"}
+            >
+              <img src="/static/img/google.png" alt="구글" className={styles.google} /> 구글로 시작하기
             </button>
-            </div>
           </div>
-
-
-        <div className="hypertext">
-=======
-        <button className={`${styles.btn1} ${styles.btnCustom}`} onClick={handleLogin}>
-          이메일로 로그인하기
-        </button>
-
-        <button
-          className={`${styles.btn2} ${styles.btnCustom}`}
-          onClick={() => window.location.href = "/oauth2/authorization/kakao"}
-        >
-          <img src="/static/img/kakao.png" alt="카카오" className={styles.kakao} />
-          카카오로 시작하기
-        </button>
-
-        <button
-          className={`${styles.btn3} ${styles.btnCustom}`}
-          onClick={() => window.location.href = "/oauth2/authorization/naver"}
-        >
-          <img src="/static/img/naver.png" alt="네이버" className={styles.naver} />
-          네이버로 시작하기
-        </button>
-
-        <div className={styles.logos}>
-          <button className={styles.btnSocial}>
-            <img src="/static/img/google.png" alt="구글" className={styles.imgSocial} />
-          </button>
-          <button className={styles.btnSocial}>
-            <img src="/static/img/facebook.png" alt="페북" className={styles.imgSocial} />
-          </button>
-          <button className={styles.btnSocial}>
-            <img src="/static/img/apple.png" alt="애플" className={styles.imgSocial} />
-          </button>
         </div>
 
         <div className={styles.hypertext}>
->>>>>>> origin/May-dev
-          <a href="/find">아이디/비밀번호 찾기</a>
-          <a href="/join">회원가입</a>
-          <a href="/mypage">마이페이지</a>
+          <a href="/find" className={styles.link}>아이디/비밀번호 찾기</a>
+          <a href="/join" className={styles.link}>회원가입</a>
+          <a href="/mypage" className={styles.link}>마이페이지</a>
         </div>
       </div>
     </div>
